c6e6686232615c5b9bbd550f5f78b0b08715ad4b
Wenxin-Jiang <wenxin_jiang@outlook.com>
2022-04-05 05:33:25 +0000

    update yolov4

--- model-benchmark/yolov4/experiment.sh
+++ model-benchmark/yolov4/experiment.sh
@@ -3,7 +3,7 @@ python3 inference.py \
   --input /home/jiang784/Benchmark/dataset/coco/val2017/ \
   --model /home/jiang784/Benchmark/models/onnx/yolov4/yolov4.onnx \
   -a /home/jiang784/Benchmark/dataset/coco/annotations/instances_val2017.json \
-  --stop 5 > output/output.txt
+  --stop 100 > output/output.txt
 
 # python inference.py \
 #   --input /home/lpmot/Dataset/COCO2017/val2017/ \
